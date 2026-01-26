7cf929c0e749624e30657bc5c8b7ab787d29eb0c
HDanielO <hameeddnl@gmail.com>
2026-01-26 11:17:25 +0100

    Add NavBar component, integrate social media icons, and update styles
    - Introduced NavBar component with a hamburger menu icon.
    - Added social media icons to the main page.
    - Updated global styles for background and font.
    - Included react-icons dependency for icon usage.
    - Removed unused favicon file.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavBar from "../components/NavBar/Navbar";
+import { FaFacebook, FaTwitter, FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
-      <p>VideoGrab</p>
+      <NavBar></NavBar>
+      <main className={styles.main}>
+        <h2 className={styles.header}>
+          Download Videos From Your Favorite Social Media
+        </h2>
+        <div className={styles.socialMediaIcons}>
+          <FaFacebook className={styles.icon}></FaFacebook>
+          <FaTwitter className={styles.icon}></FaTwitter>
+          <FaInstagram className={styles.icon}></FaInstagram>
+          <FaTiktok className={styles.icon}></FaTiktok>
+        </div>
+        
+        <select className={styles.select}>
+          <option disabled selected>
+            Select Options
+          </option>
+          <option value="facebook">Facebook</option>
+          <option value="twitter">Twitter</option>
+          <option value="instagram">Instagram</option>
+          <option value="tiktok">TikTok</option>
+        </select>
+      </main>
     </div>
   );
 }
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,13 @@
+import styles from "./Navbar.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const NavBar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>VIDEOGRAB</div>
+      <GiHamburgerMenu className={styles.menu} />
+    </div>
+  );
+};
+
+export default NavBar;
